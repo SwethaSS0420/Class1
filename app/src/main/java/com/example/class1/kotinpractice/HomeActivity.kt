39c74fc7344bd2ff9c44db4a1dfcdf7213adf2a6
SwethaSS0420 <swetha.driram2022@vitstudent.ac.in>
2024-06-20 10:27:17 +0530

    Mars images on recycler view

--- app/src/main/java/com/example/class1/kotinpractice/HomeActivity.kt
+++ app/src/main/java/com/example/class1/kotinpractice/HomeActivity.kt
@@ -5,17 +5,30 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.class1.network.MarsAdapter
 import com.example.class1.network.MarsApi
+import com.example.class1.network.MarsPhoto
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class HomeActivity : AppCompatActivity(){
-    var TAG = HomeActivity::class.java.simpleName
+    var TAG = HomeActivity::class.java.simpleName    //"HomeActivity"
+
+    lateinit var marsRecyclerView:RecyclerView
+    lateinit var marsAdapter: MarsAdapter
+    lateinit var photos:List<MarsPhoto>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
+        marsRecyclerView = findViewById(R.id.recyclerViewUrls)
+        marsRecyclerView.layoutManager = LinearLayoutManager(this)
+        photos = ArrayList()
+        marsAdapter = MarsAdapter(photos)
+        marsRecyclerView.adapter = marsAdapter
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -25,11 +38,21 @@ class HomeActivity : AppCompatActivity(){
 
 
     }
-    private fun getMarsPhotos() {
-        GlobalScope.launch {
 
+
+    private fun getMarsPhotos() {
+        GlobalScope.launch(Dispatchers.Main) {
+            //launching coroutines on the main thread is not advisable
             var listMarsPhotos =   MarsApi.retrofitService.getPhotos()
+            // photos = listMarsPhotos
+            marsAdapter.listMarsPhotos = listMarsPhotos
+            marsAdapter.notifyDataSetChanged()
+            //   var tvHome:TextView = findViewById(R.id.tvHome)
+//            tvHome.setText(listMarsPhotos.get(1).imgSrc)
             Log.i("homeactiviy",listMarsPhotos.size.toString())
+            Log.i("homeactivity-url",listMarsPhotos.get(1).imgSrc)
+
+
         }
     }
 
@@ -38,4 +61,3 @@ class HomeActivity : AppCompatActivity(){
     }
 
 }
-
